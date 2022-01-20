fe5af1f8e07c728f67281fad7bda3a1de3955b73
Ebennezer <88779339+Ebennezer@users.noreply.github.com>
2022-01-20 10:25:09 +0300

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,6 @@
+function myFunctions() {
+    document.getElementById("password").style.backgroundColor = "blue";
+  }
+  function blurFunction() {
+    document.getElementById("password").style.background = "transparent";
+  }
